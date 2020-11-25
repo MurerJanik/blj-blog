2ba6e7587b0ff4fff2b31a77994b55998b3776c7
Janik Murer <janik.murer@gmx.ch>
2020-11-25 10:57:28 +0100

    Edited home

--- blog.php
+++ blog.php
@@ -6,31 +6,40 @@
     <title>blog.php</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body class="everything">
+<body>
 
-<div>
-    <h3>Mein Blog</h3>
-    
-</div>
+<nav class="nav">
+    <ul>
+    <li><a href="/blj/blog/home.php">Home</a></li>
+    <li><a class="blog_button" href="/blj/blog/blog.php">Blog</a></li>
+    </ul>
+</nav>
 
 
 <form action="blog.php" method="post"> 
+    
     <div class="name">  
         <label for="text">Name:</label>
         <textarea class="textfeld" id="name" name="name" cols="40" rows="2"></textarea> 
-      
    </div> 
 
-    <div class="text">
-         <label for="text">Text:</label>      
-         <textarea class="textfeld2" name="text" id="text" cols="40" rows="15"></textarea>
+   <div class="titel">
+    <label for="text">Titel:</label>
+    <textarea class="textfeldtitel" name="textfeld" id="titel" cols="40" rows="2"></textarea>
    </div>
 
-     <input class="senden" type="submit" value="Senden" />
-   
+    <div class="text">
+         <label for="text">Text:</label>
+         <textarea class="textfeld2" name="text" id="text" cols="40" rows="15"></textarea>
+   </div>
+    <div class="submit">
+        <input  type="submit" value="Senden" />
+    </div>
 </form> 
 
-<div class="border"> </div>
+<div class="border"> 
+    
+</div>
 
 
 </body>
---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home.php</title>
+    <link rel="stylesheet" href="css/style1.css">
+</head>
+<body>
+    <nav class="nav">
+        <ul class="ul">
+        <li class="li"><a href="/blj/blog/home.php">Home</a></li>
+        <li class="li"><a class="blog_button" href="/blj/blog/blog.php">Blog</a></li>
+        </ul>
+    </nav>
+
+    <div class="titel">
+        <h1>Welcome to my Blog</h1>
+    </div>
+
+    <div class="rules">
+        <h2>Rules:</h2>
+    </div>
+
+    <div class="textrules">
+        <ul>
+            <li><h3>No insults</h3></li>
+            <li><h3>No racist or discriminatory statements</h3></li>
+            <li><h3>No Darvin</h3></li>
+            <li><h3>Please write in English</h3></li>
+            <li><h3>Only constructive criticism</h3></li>
+            <li><h3>No contributions glorifying violence</h3></li>
+            <li><h3>Do not write anything untrue.</h3></li>
+            <li><h3>Write clearly and unambiguously.</h3></li>
+        </ul>
+    </div>
+
+</body>
+</html>
